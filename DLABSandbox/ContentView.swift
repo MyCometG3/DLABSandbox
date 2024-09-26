88fccb69c551d66108b6c587b3759612654897d0
Takashi Mochizuki <mycometg3@icloud.com>
2024-09-26 09:39:45 +0900

    Support new onChange() from macOS 14

--- DLABSandbox/ContentView.swift
+++ DLABSandbox/ContentView.swift
@@ -24,21 +24,27 @@ struct ContentView: View {
             HStack {
                 VStack {
                     HStack {
-                        Picker("", selection: $controller.modeSelectedIndex) {
+                        let pickerMode = Picker("", selection: $controller.modeSelectedIndex) {
                             Text("0").tag(0)
                             Text("1").tag(1)
                             Text("2").tag(2)
                             Text("3").tag(3)
                             Text("4").tag(4)
                         }
-                        .onChange(of: controller.modeSelectedIndex, perform: { newValue in
-                            controller.selectPreset(newPreset: newValue)
-                        })
                         .controlSize(.small)
                         .pickerStyle(SegmentedPickerStyle())
                         .disabled(controller.running)
-                        
-                        Picker("", selection: $controller.layoutSelectedIndex) {
+                        if #available(macOS 14.0, *) {
+                            pickerMode.onChange(of: controller.modeSelectedIndex) { _, newValue in
+                                controller.selectPreset(newPreset: newValue)
+                            }
+                        } else {
+                            pickerMode.onChange(of: controller.modeSelectedIndex) { newValue in
+                                controller.selectPreset(newPreset: newValue)
+                            }
+                        }
+
+                        let pickerLayout = Picker("", selection: $controller.layoutSelectedIndex) {
                             Text("N/A").tag(0)
                             Text("C").tag(1)
                             Text("L R").tag(2)
@@ -46,12 +52,18 @@ struct ContentView: View {
                             Text("5.1ch").tag(6)
                             Text("7.1ch").tag(8)
                         }
-                        .onChange(of: controller.layoutSelectedIndex, perform: { newValue in
-                            controller.selectLayout(newLayout: newValue)
-                        })
                         .controlSize(.small)
                         .pickerStyle(SegmentedPickerStyle())
                         .disabled(controller.running)
+                        if #available(macOS 14.0, *) {
+                            pickerLayout.onChange(of: controller.layoutSelectedIndex) { _, newValue in
+                                controller.selectLayout(newLayout: newValue)
+                            }
+                        } else {
+                            pickerLayout.onChange(of: controller.layoutSelectedIndex) { newValue in
+                                controller.selectLayout(newLayout: newValue)
+                            }
+                        }
                     }
                     
                     HStack {
@@ -64,22 +76,34 @@ struct ContentView: View {
                 
                 VStack {
                     HStack {
-                        Toggle(isOn: $controller.running) {
+                        let toggleRunning = Toggle(isOn: $controller.running) {
                             Label("Capture", systemImage: "power.circle").font(.headline)
                         }
-                        .onChange(of: controller.running) { newValue in
-                            controller.toggleRunning()
-                        }
                         .toggleStyle(.button)
-                        
-                        Toggle(isOn: $controller.recording) {
-                            Label("Record", systemImage: "record.circle").font(.headline)
+                        if #available(macOS 14.0, *) {
+                            toggleRunning.onChange(of: controller.running) {
+                                controller.toggleRunning()
+                            }
+                        } else {
+                            toggleRunning.onChange(of: controller.running) { _ in
+                                controller.toggleRunning()
+                            }
                         }
-                        .onChange(of: controller.recording) { newValue in
-                            controller.toggleRecording()
+                        
+                        let toggleRecording = Toggle(isOn: $controller.recording) {
+                            Label("Record", systemImage: "record.circle").font(.headline)
                         }
                         .toggleStyle(.button)
                         .disabled(controller.running == false)
+                        if #available(macOS 14.0, *) {
+                            toggleRecording.onChange(of: controller.recording) {
+                                controller.toggleRecording()
+                            }
+                        } else {
+                            toggleRecording.onChange(of: controller.recording) { _ in
+                                controller.toggleRecording()
+                            }
+                        }
                     }
                     HStack {
                         Text("model:").font(.footnote)
